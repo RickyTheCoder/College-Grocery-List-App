5d619ecca76b437a842710ee6c94876d204255a0
RickyTheCoder <rivera.ricky8@gmail.com>
2024-04-11 14:34:50 -0500

    GroceryList

---
+++ College Grocery List App/College Grocery List App/src/GroceryList.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroceryList {
+    private final List<GroceryItemOrder> orders;
+
+    public GroceryList() {
+        orders = new ArrayList<>();
+    }
+
+    // Add a grocery item order to the list
+    public boolean add(GroceryItemOrder order) {
+        return orders.add(order);
+    }
+
+    // Calculate the total cost of all orders in the list
+    public double getTotalCost() {
+        return orders.stream().mapToDouble(GroceryItemOrder::getCost).sum();
+    }
+
+    // Override the toString method to get a string representation of the list
+    @Override
+    public String toString() {
+        return orders.stream()
+                .map(order -> order.getItem().getName() + " x" + order.getQuantity())
+                .collect(Collectors.joining(", ", "[", "]"));
+    }
+}
