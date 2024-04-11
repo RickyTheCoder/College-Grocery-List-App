1875c7d13bedbabf8bc9285a32d0d050e2192600
Evan <evanpolk2@gmail.com>
2024-04-11 14:57:11 -0500

    Running main with no error

--- College Grocery List App/College Grocery List App/src/GroceryList.java
+++ College Grocery List App/College Grocery List App/src/GroceryList.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GroceryList {
     private final List<GroceryItemOrder> orders;
--- College Grocery List App/College Grocery List App/src/Shopper.java
+++ College Grocery List App/College Grocery List App/src/Shopper.java
@@ -4,9 +4,9 @@ import java.util.List;
 public interface Shopper {
     List<GroceryList> groceryLists = new ArrayList<>();
 
-    public default GroceryList recordList(GroceryStore groceryStore) {
-        GroceryList list = new GroceryList(groceryStore);
-        groceryLists.add(new GroceryList(groceryStore));
+    public default GroceryList recordList() {
+        GroceryList list = new GroceryList();
+        groceryLists.add(list);
         return list;
     }
 
