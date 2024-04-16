87910da39be459106a4f354d86b75019e360fc3a
andrewrowe <arowe@luc.edu>
2024-04-16 11:42:12 -0500

    Andrew

---
+++ College Grocery List App/College Grocery List App/src/Professor.java
@@ -0,0 +1,16 @@
+public class Professor extends Person {
+
+    private int employeeId;
+
+    public Professor(String familyName, String givenNames, int day, int month, int year, boolean isMale) {
+        super(familyName, givenNames, day, month, year, isMale);
+
+    }
+
+    /**
+     *
+     * Add discount method for professor...even though doesn't get one
+     *
+     */
+
+}
